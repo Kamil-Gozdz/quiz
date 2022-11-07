b9ec8995a7f8e778f5ba09ea2f27f62bda7def58
Kamil <kamilgozdz23@gmail.com>
2022-11-07 22:34:15 +0100

    feature/Handling special signs in browser

--- src/main/java/pl/sdaacademy/projectplus/quiz/dto/QuestionsDto.java
+++ src/main/java/pl/sdaacademy/projectplus/quiz/dto/QuestionsDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -28,5 +29,17 @@ public class QuestionsDto {
         @JsonProperty("incorrect_answers")
         private List<String> incorrectAnswers;
 
+        public void setQuestion(String question) {
+            this.question = HtmlUtils.htmlUnescape(question);
+        }
+
+        public void setCorrectAnswer(String correctAnswer) {
+            this.correctAnswer = HtmlUtils.htmlUnescape(correctAnswer);
+        }
+
+        public void setIncorrectAnswers(List<String> incorrectAnswers){
+            this.incorrectAnswers = incorrectAnswers.stream()
+                    .map(HtmlUtils::htmlUnescape).toList();
+        }
     }
 }
